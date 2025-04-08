3487e979efa30ec717136481510a53749be5d049
Adi <asanand1297@gmail.com>
2025-04-08 12:31:07 +0530

    updated typo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
-import SignUpPage from "./pages/auth/signup/SignUpPage";
+
 import NotificationPage from "./pages/notification/NotificationPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 
@@ -12,6 +12,7 @@ import RightPanel from "./components/common/RightPanel";
 import { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
+import SignUpPage from "./pages/auth/signup/SignupPage";
 
 function App() {
   const { data: authUser, isLoading } = useQuery({
