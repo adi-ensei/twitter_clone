383a11a6310fcf0d9924f8a768bd0fbb235ee109
adi-ensei <139208602+adi-ensei@users.noreply.github.com>
2025-04-08 12:26:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 
-import HomePage from "./pages/home/HomePage";
+import HomePage from "./pages/Home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import NotificationPage from "./pages/notification/NotificationPage";
